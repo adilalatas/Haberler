bab4782ac43f57a3f584ee42b9918aad2292db1b
adilalatas <adil.alatas38@gmail.com>
2023-03-19 13:44:46 +0300

    düzeltme

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -18,9 +18,9 @@ export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-xl bg-body-tertiary " id="navbar">
       <div className="container-fluid ">
-        <span className="navbar-brand" >
+        <a className="navbar-brand" href='/' >
           <h2> {t('Haberler')}</h2>
-        </span>
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
           <span className="navbar-toggler-icon"></span>
--- src/componenets/pages/Category.js
+++ src/componenets/pages/Category.js
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 export default function Category(props) {
   const [cards, setCards] = useState([]);
   useEffect(() => {
-   const apiUrl = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}`;
+    let apiUrl = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}`;
     if (props.search) {
       apiUrl = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&q=${props.search}&apiKey=${props.apiKey}`;
     }
@@ -14,7 +14,8 @@ export default function Category(props) {
         setCards(data.articles);
         console.log(data.articles);
       });
-  }, [props.country, props.category, props.search]);
+  }, [props.country, props.category, props.search, props.apiKey]); // apiKey değişkenini de bağımlılık dizisine ekledik.
+  
   const { t } = useTranslation();
   return (
     <div className="container text-center my-2">
@@ -25,17 +26,17 @@ export default function Category(props) {
         {cards.map((article, index) => {
           return (
             <div key={index} className="col-md-4 mb-3">
-            <div className="card text-center" style={{ width: "100%" }}>
-              <img src={article.urlToImage} className="card-img-top" alt="" />
-              <div className="card-body">
-                <h5 className="card-title">{article.title}</h5>
-                <p className="card-text">{article.description}</p>
-                <a href={article.url} className="btn btn-primary">
-                  {t("ReadMore")}
-                </a>
+              <div className="card text-center" style={{ width: "100%" }}>
+                <img src={article.urlToImage} className="card-img-top" alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">{article.title}</h5>
+                  <p className="card-text">{article.description}</p>
+                  <a href={article.url} className="btn btn-primary">
+                    {t("ReadMore")}
+                  </a>
+                </div>
               </div>
-            </div>
-          </div>  
+            </div>  
           );
         })}
       </div>
